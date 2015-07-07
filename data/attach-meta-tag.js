6c2909f83d7841b6987d7d43adfc554612e05e3d
akhileshwar.guli <akhileshwar.guli@gmail.com>
2015-07-07 03:15:34 -0500

    change in storePassAsync()

--- data/attach-meta-tag.js
+++ data/attach-meta-tag.js
@@ -6,5 +6,5 @@ self.port.on("start", function (tagID) {
 	var x = document.createElement("meta"); 
 	x.id = tagID;
 	document.head.appendChild(x);
-	console.log("attach-meta-tag.js: "+tagID+" ATTACHED to "+document.URL);
+	//console.log("attach-meta-tag.js: "+tagID+" ATTACHED to "+document.URL);
 });
--- data/wait-on-tag.js
+++ data/wait-on-tag.js
@@ -2,7 +2,7 @@ const max_count = 9999;
 
 self.port.on("start", function (tagID) {
 	var count = 0;
-	console.log("wait-on-tag.js: START tag: "+tagID+" TAB: "+document.URL);
+	//console.log("wait-on-tag.js: START tag: "+tagID+" TAB: "+document.URL);
 	if (tagID == null) {
 		self.port.emit("error", "wait-on-tag.js: tagID was null");
 		return;
@@ -10,10 +10,10 @@ self.port.on("start", function (tagID) {
 	while(document.getElementById(tagID) == null && count < max_count) {
 		count++;
 	}
-	console.log("wait-on-tag.js: Complete count: "+count);
+	/* console.log("wait-on-tag.js: Complete count: "+count);
 	console.log("wait-on-tag.js: "+document.getElementById(tagID));
 	var temp = document.getElementsByTagName("meta");
-	console.log("wait-on-tag.js: meta Length: "+temp.length);
+	console.log("wait-on-tag.js: meta Length: "+temp.length); */
 	if (document.getElementById(tagID) == null) {
 		self.port.emit("error", "wait-on-tag.js: tagID not found after "+max_count+" iterations.");
 	} else {
